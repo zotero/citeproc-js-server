8c5b92565e495b3c9f6f189fdb8e28000a794ffb
Faolan C-P <fcheslack@gmail.com>
2010-12-12 20:44:06 -0500

    use Step to pipeline requests through async calls. minor reorganization and comments

--- citeServer.js
+++ citeServer.js
@@ -1,29 +1,33 @@
-var repl = require('repl');
+//include required builtin modules
+//var repl = require('repl');
 var fs = require('fs');
 var http = require('http');
 var url = require('url');
-var Step = require('./step');
 
-var zcite = {
-};
+//global namespace citation server variable
+var zcite = {};
 global.zcite = zcite;
 
 zcite.config = JSON.parse(fs.readFileSync('./citeServerConf.json', 'utf8'));
-zcite.listenport = 8085;
 
+//process command line args
 var args = process.argv;
 for(var i = 1; i < args.length; i++){
     if(args[i].substr(0, 4) == 'port'){
-        zcite.listenport = parseInt(args[i].substr(5));
+        zcite.config.listenport = parseInt(args[i].substr(5));
     }
-
 }
 
-var parser = require('./node-o3-xml/lib/o3-xml');
-//var parser = require('/usr/local/node/o3-xml');
-zcite.CSL = require('./citeprocmodule').CSL;
-zcite.cslFetcher = require('./cslFetcher').cslFetcher;
+//load non-builtin modules using paths from config
+var Step = require(zcite.config.stepPath);
+var parser = require(zcite.config.parserPath);
 
+zcite.CSL = require(zcite.config.citeprocmodulePath).CSL;
+zcite.cslFetcher = require(zcite.config.cslFetcherPath).cslFetcher;
+zcite.cslFetcher.init(zcite.config);
+
+//set up debug/logging output
+//logging, especially of errors, should be changed to be more consistent with other server log formats
 if(zcite.config.debugLog == false){
     console.log("no debugLog");
     zcite.log = function(m){};
@@ -54,40 +58,45 @@ zcite.debug = function(m, level){
 
 //zcite exception response function
 zcite.respondException = function(err, response, statusCode){
+    console.log("respondException");
+    zcite.debug(err, 3);
     if(typeof statusCode == 'undefined'){
         var statusCode = 500;
     }
     if(typeof response != "undefined"){
         if(typeof err == "string"){
             response.writeHead(statusCode, {'Content-Type': 'text/plain'});
-            response.end(err);
+            response.end("An error occurred");
+            zcite.debug("caught exception : " + err, 1);
             return;
         }
         else{
             response.writeHead(statusCode, {'Content-Type': 'text/plain'});
             response.end("An error occurred");
+            zcite.debug(err, 1);
             return;
         }
     }
     if(typeof err == "string"){
-        zcite.log("unCaught exception: " + err);
+        //zcite.log("unCaught exception: " + err);
         zcite.debug("unCaught exception: " + err, 1);
     }
     else{
         zcite.debug('unCaught exception: ' + err.name + " : " + err.message, 1);
-        zcite.log('unCaught exception: ' + err.name + " : " + err.message);
+        //zcite.log('unCaught exception: ' + err.name + " : " + err.message);
     }
 };
 
 //preload locales into memory
-zcite.localesDirName = "./csl-locales/trunk";
-zcite.localesDir = fs.readdirSync(zcite.localesDirName);
+zcite.localesDir = fs.readdirSync(zcite.config.localesPath);
 
 zcite.locales = {};
 for(var i = 0; i < zcite.localesDir.length; i++){
     var localeCode = zcite.localesDir[i].slice(8, 13);
-    zcite.locales[localeCode] = fs.readFileSync(zcite.localesDirName + '/' + zcite.localesDir[i], 'utf8');
+    zcite.locales[localeCode] = fs.readFileSync(zcite.config.localesPath + '/' + zcite.localesDir[i], 'utf8');
 }
+
+//retrieveLocale function for use by citeproc Engine
 zcite.retrieveLocale = function(lang){
     var locales = zcite.locales;
     if(locales.hasOwnProperty(lang)){
@@ -105,7 +114,7 @@ zcite.cslXml = {};
 //key is style, lang
 zcite.cachedEngines = {};
 
-zcite.createEngine = function(zcreq){
+zcite.createEngine = function(zcreq, callback){
     //console.log(zcreq);
     zcite.debug('zcite.createEngine', 5);
     var cpSys = {
@@ -114,12 +123,13 @@ zcite.createEngine = function(zcreq){
         retrieveItem: function(itemID){return this.items[itemID];}
     };
     zcite.debug("cpSys created", 5);
+    zcite.debug(zcreq.config.locale, 5);
     var citeproc = new zcite.CSL.Engine(cpSys, zcreq.cslXml, zcreq.config.locale);
     zcite.debug('engine created', 5);
     zcreq.citeproc = citeproc;
     //run the actual request now that citeproc is initialized (need to run this from cacheLoadEngine instead?)
     if(!zcite.precache){
-        zcite.runRequest(zcreq);
+        callback(null, zcreq);
     }
     else{
         citeproc.sys.items = {};
@@ -142,16 +152,23 @@ zcite.cacheLoadEngine = function(styleUri, locale){
         }
         else{
             var citeproc = zcite.cachedEngines[cacheEngineString].pop();
+            citeproc.sys.items = {};
+            citeproc.updateItems([]);
+            citeproc.restoreProcessorState();
             return citeproc;
         }
     }
     else{
         var citeproc = zcite.cachedEngines[cacheEngineString];
+        citeproc.sys.items = {};
+        citeproc.updateItems([]);
+        citeproc.restoreProcessorState();
         delete zcite.cachedEngines[cacheEngineString];
         return citeproc;
     }
 };
 
+//save a csl engine specified by styleuri:locale
 zcite.cacheSaveEngine = function(citeproc, styleUri, locale){
     zcite.debug('zcite.cacheSaveEngine', 5);
     var cacheEngineString = styleUri + ':' + locale;
@@ -223,7 +240,8 @@ Step(
             };
         zcite.cslFetcher.fetchStyle(zcreq, this);
     },
-    function initEngine(zcreq){
+    function initEngine(err, zcreq){
+        if(err) throw err;
         zcite.createEngine(zcreq);
         return true;
     },
@@ -267,7 +285,7 @@ Step(
         return true;
     },
 */    function disablePrecache(err, success){
-        zcite.debug("disablePrecache", 5);
+        zcite.debug("turn precache flag off", 5);
         if(err) throw err;
         zcite.precache = false;
         return true;
@@ -282,68 +300,73 @@ Step(
 
 //callback for when engine is fully initialized and ready to process the request
 zcite.runRequest = function(zcreq){
-    zcite.debug('zcite.runRequest', 5);
-    var response = zcreq.response;
-    var citeproc = zcreq.citeproc;
-    var config = zcreq.config;
-    var responseJson = {};
-    
-    //delete zcreq.citeproc;
-    //zcite.debug(zcreq, 5);
-    //set output format
-    if(config.outputformat != "html"){
-        citeproc.setOutputFormat(config.outputformat);
-    }
-    zcite.debug("outputFormat set", 5);
-    //add items posted with request
-    citeproc.updateItems(zcreq.reqItemIDs);
-    if(citeproc.opt.sort_citations){
-        zcite.debug("currently using a sorting style", 1);
-    }
-    zcite.debug("items Updated", 5);
-    
-    //switch process depending on bib or citation
-    if(config.bibliography == "1"){
-        zcite.debug('generating bib', 5);
-        var bib = citeproc.makeBibliography();
-        zcite.debug("bib generated", 5);
-        responseJson.bibliography = bib;
-    }
-    if(config.citations == "1"){
-        zcite.debug('generating citations', 5);
-        var citations = [];
-        for(var i = 0; i < zcreq.citationClusters.length; i++){
-            citations.push(citeproc.appendCitationCluster(zcreq.citationClusters[i], true)[0]);
+    try{
+        zcite.debug('zcite.runRequest', 5);
+        var response = zcreq.response;
+        var citeproc = zcreq.citeproc;
+        var config = zcreq.config;
+        var responseJson = {};
+        
+        //delete zcreq.citeproc;
+        //zcite.debug(zcreq, 5);
+        //set output format
+        if(config.outputformat != "html"){
+            citeproc.setOutputFormat(config.outputformat);
         }
-        zcite.debug(citations, 5);
-        responseJson.citations = citations;
-    }
-    
-    var write = '';
-    //write the CSL output to the http response
-    if(config.responseformat == "json"){
-        response.writeHead(200, {'Content-Type': 'application/json'});
-        write = JSON.stringify(responseJson);
-    }
-    else{
-        if(config.outputformat == 'html'){
-            response.writeHead(200, {'Content-Type': 'text/html'});
+        zcite.debug("outputFormat set", 5);
+        //add items posted with request
+        citeproc.updateItems(zcreq.reqItemIDs);
+        if(citeproc.opt.sort_citations){
+            zcite.debug("currently using a sorting style", 1);
         }
-        else if(config.outputformat == 'rtf'){
-            response.writeHead(200, {'Content-Type': 'text/rtf'});
+        zcite.debug("items Updated", 5);
+        
+        //switch process depending on bib or citation
+        if(config.bibliography == "1"){
+            zcite.debug('generating bib', 5);
+            var bib = citeproc.makeBibliography();
+            zcite.debug("bib generated", 5);
+            responseJson.bibliography = bib;
         }
-        //not sure yet what should actually be written here, but will just do assembled bib for now
-        if(bib){
-            write += bib[0].bibstart + bib[1].join('') + bib[0].bibend;
+        if(config.citations == "1"){
+            zcite.debug('generating citations', 5);
+            var citations = [];
+            for(var i = 0; i < zcreq.citationClusters.length; i++){
+                citations.push(citeproc.appendCitationCluster(zcreq.citationClusters[i], true)[0]);
+            }
+            zcite.debug(citations, 5);
+            responseJson.citations = citations;
         }
+        
+        var write = '';
+        //write the CSL output to the http response
+        if(config.responseformat == "json"){
+            response.writeHead(200, {'Content-Type': 'application/json'});
+            write = JSON.stringify(responseJson);
+        }
+        else{
+            if(config.outputformat == 'html'){
+                response.writeHead(200, {'Content-Type': 'text/html'});
+            }
+            else if(config.outputformat == 'rtf'){
+                response.writeHead(200, {'Content-Type': 'text/rtf'});
+            }
+            //not sure yet what should actually be written here, but will just do assembled bib for now
+            if(bib){
+                write += bib[0].bibstart + bib[1].join('') + bib[0].bibend;
+            }
+        }
+        
+        response.write(write, 'utf8');
+        response.end();
+        zcite.debug("response sent", 5);
+        
+        citeproc.sys.items = {};
+        zcite.cacheSaveEngine(zcreq.citeproc, zcreq.styleUrlObj.href, zcreq.config.locale);
     }
-    
-    response.write(write, 'utf8');
-    response.end();
-    zcite.debug("response sent", 5);
-    
-    citeproc.sys.items = {};
-    zcite.cacheSaveEngine(zcreq.citeproc, zcreq.styleUrlObj.href, zcreq.config.locale);
+    catch(err){
+        zcite.respondException(err, zcreq.response);
+    }
 };
 
 zcite.configureRequest = function(uriConf){
@@ -364,6 +387,7 @@ zcite.configureRequest = function(uriConf){
 }
 
 http.createServer(function (request, response) {
+    //zcreq keeps track of information about this request and is passed around
     var zcreq = {};
     console.log("request received");
     if(request.method == "OPTIONS"){
@@ -395,13 +419,15 @@ http.createServer(function (request, response) {
     request.on('end', function(){
         try{
             zcite.debug('full request received', 5);
+            //parse url from request object
             var uriObj = url.parse(this.url);
             uriObj.parsedQuery = require('querystring').parse(uriObj.query);
             zcite.debug(uriObj, 5);
+            //make config obj based on query
             var config = zcite.configureRequest(uriObj.parsedQuery);
-            //zcite.log('request : ' + config.responseformat + ' : ' + config.bibliography + ' : ' + config.citations + ' : ' + config.outputformat + ' : ' + config.style + ' : ' + config.locale);
             zcite.debug(JSON.stringify(config), 4);
             zcreq.config = config;
+            //need to keep response in zcreq so async calls stay tied to a request
             zcreq.response = response;
             try{
                 var postObj = JSON.parse(this.POSTDATA);
@@ -425,6 +451,7 @@ http.createServer(function (request, response) {
             }
             
             //push itemIDs onto array and id referenced object for updateItems and retrieveItem function
+            //items can be passed in as an object with keys becoming IDs, but ordering will not be guarenteed
             if(reqItems instanceof Array){
                 //console.log(reqItems);
                 for(var i = 0; i < reqItems.length; i++){
@@ -446,34 +473,101 @@ http.createServer(function (request, response) {
                 }
             }
             
+            //add citeproc required functions to zcreq object so it can be passed into CSL.Engine constructor
             zcreq.retrieveLocale = global.zcite.retrieveLocale;
             zcreq.retrieveItem = function(itemID){return this.items[itemID];};
+            
             zcreq.reqItemIDs = reqItemIDs;
             zcreq.reqItemsObj = reqItemsObj;
-            zcreq.styleUrlObj = zcite.cslFetcher.processStyleIdentifier(zcreq.config.style);
+            
             if(config.citations == '1'){
                 zcreq.citationClusters = zcreq.postObj.citationClusters;
             }
             
-            //check for cached version or create new CSL Engine
-            var citeproc;
-            if(citeproc = zcite.cacheLoadEngine(zcreq.styleUrlObj.href, zcreq.config.locale)){
-                citeproc.sys.items = zcreq.reqItemsObj;
-                zcite.debug("citeproc.sys.items reset for zcreq", 5);
-                zcreq.citeproc = citeproc;
-                zcite.runRequest(zcreq);
-            }
-            else{
-                var cslXml;
-                if(cslXml = zcite.cslFetcher.getCachedStyle(zcreq.styleUrlObj.href)){
-                    //successfully fetched cached style - load engine and run request
-                    zcrec.cslXml = cslXml;
-                    zcite.createEngine(zcreq);
+            //make style identifier so we can check caches for real
+            //check for citeproc engine cached
+            //otherwise check for cached style
+            //-initialize 
+            Step(
+                function fetchStyleIdentifier(){
+                    //put the passed styleUrl into a standard form (adding www.zotero.org to short names)
+                    zcite.debug("request step: fetchStyleIdentifier", 5);
+                    //console.log(zcreq);
+                    zcreq.styleUrlObj = zcite.cslFetcher.processStyleIdentifier(zcreq.config.style);
+                    return zcreq;
+                },
+                function tryCachedEngine(err, zcreq){
+                    if(err){
+                        zcite.debug("rethrowing error in tryCachedEngine");
+                        throw err;
+                    }
+                    zcite.debug("request step: tryCachedEngine", 5);
+                    //check for cached version or create new CSL Engine
+                    var citeproc;
+                    if(citeproc = zcite.cacheLoadEngine(zcreq.styleUrlObj.href, zcreq.config.locale)){
+                        citeproc.sys.items = zcreq.reqItemsObj;
+                        zcite.debug("citeproc.sys.items reset for zcreq", 5);
+                        zcreq.citeproc = citeproc;
+                    }
+                    
+                    return zcreq;
+                },
+                function fetchStyle(err, zcreq){
+                    zcite.debug("request step: fetchStyle", 5);
+                    if(err){
+                        zcite.debug("rethrowing error in fetchStyle");
+                        throw err;
+                    }
+                    if(typeof zcreq.citeproc != 'undefined'){
+                        //have cached engine, don't need csl xml
+                        return zcreq;
+                    }
+                    var cslXml;
+                    if(cslXml = zcite.cslFetcher.getCachedStyle(zcreq.styleUrlObj.href)){
+                        //successfully fetched cached style - load engine and run request
+                        zcreq.cslXml = cslXml;
+                        return zcreq;
+                    }
+                    else{
+                        zcite.cslFetcher.fetchStyle(zcreq, this);
+                    }
+                },
+                function createEngine(err, zcreq){
+                    zcite.debug("request step: createEngine", 5);
+                    if(err){
+                        zcite.debug("rethrowing error in createEngine");
+                        throw err;
+                    }
+                    //zcite.debug("cslXml: " + zcreq.cslXml, 5);
+                    if(typeof zcreq.citeproc != 'undefined'){
+                        //have cached engine, don't need csl xml
+                        zcite.debug("still no cached engine", 5);
+                        return zcreq;
+                    }
+                    zcite.createEngine(zcreq, this);
+                },
+                function runRequest(err, zcreq){
+                    zcite.debug("request step: runRequest", 5);
+                    if(err){
+                        zcite.debug("rethrowing error in runRequest");
+                        throw err;
+                    }
+                    //console.log(zcreq);
+                    zcite.runRequest(zcreq, this);
+                },
+                function catchError(err, zcreq){
+                    zcite.debug("request step: catchError", 5);
+                    if(err) {
+                        zcite.debug("error in Step", 5);
+                        zcite.respondException(err, zcreq.response);
+                    }
+                    else{
+                        zcite.debug("request step finished without apparent error", 5);
+                    }
                 }
-                else{
-                    zcite.cslFetcher.fetchStyle(zcreq, zcite.createEngine);
-                }
-            }
+            );
+            
+            
         }
         catch(err){
             if(typeof err == "string"){
@@ -493,9 +587,9 @@ http.createServer(function (request, response) {
         zcite.debug("100-continue expected. writing header to response");
         response.writeHead(100);
     }
-}).listen(zcite.listenport);
+}).listen(zcite.config.listenport);
 
-zcite.debug('Server running at http://127.0.0.1:' + zcite.listenport + '/', 1);
+zcite.debug('Server running at http://127.0.0.1:' + zcite.config.listenport + '/', 1);
 
 process.on('uncaughtException', function (err) {
     zcite.respondException(err, response);
--- cslFetcher.js
+++ cslFetcher.js
@@ -2,18 +2,40 @@ var fs = require('fs');
 var http = require('http');
 var url = require('url');
 
-
 var cslFetcher = {
     '_cache':{}
 };
 
-
-cslFetcher.cslDirName = "./csl1.0";
-cslFetcher.cslDir = fs.readdirSync(cslFetcher.cslDirName);
-cslFetcher.cslShortNames = {};
-for(var i = 0; i < cslFetcher.cslDir.length; i++){
-    var shortName = cslFetcher.cslDir[i].slice(0, -4);
-    cslFetcher.cslShortNames[shortName] = true;
+cslFetcher.init = function(config){
+    if(typeof config == 'undefined'){
+        config = {
+            "localesPath" : "./csl-locales/trunk",
+            "cslPath" : "./csl1.0",
+            "parserPath" : "../nodejs/node-o3-xml/lib/o3-xml",
+            "stepPath" : "./step"
+        }
+    }
+    
+    Step = require(config.stepPath);
+    cslFetcher.parser = require(config.parserPath);
+    
+    cslFetcher.cslPath = config.cslPath;
+    cslFetcher.cslDir = fs.readdirSync(cslFetcher.cslPath);
+    cslFetcher.cslDependentDir = fs.readdirSync(cslFetcher.cslPath + '/dependent');
+    cslFetcher.cslShortNames = {};
+    cslFetcher.cslDependentShortNames = {};
+    //map short names that we have independent styles for
+    for(var i = 0; i < cslFetcher.cslDir.length; i++){
+        var shortName = cslFetcher.cslDir[i].slice(0, -4);
+        cslFetcher.cslShortNames[shortName] = true;
+    }
+    
+    //map short names that we have dependent styles for
+    for(var i = 0; i < cslFetcher.cslDependentDir.length; i++){
+        var shortName = cslFetcher.cslDependentDir[i].slice(0, -4);
+        cslFetcher.cslDependentShortNames[shortName] = true;
+    }
+    return true;
 }
 
 cslFetcher.getCachedStyle = function(url){
@@ -26,7 +48,7 @@ cslFetcher.getCachedStyle = function(url){
     }
 }
 
-cslFetcher.processStyleIdentifier = function(style){
+cslFetcher.processStyleIdentifier = function(style, callback){
     var urlObj = url.parse(style);
     if(typeof urlObj.host == "undefined"){
         //short name, treat as a zotero.org/styles url
@@ -42,38 +64,68 @@ cslFetcher.processStyleIdentifier = function(style){
     return urlObj;
 };
 
-cslFetcher.fetchStyle = function(zcreq, callback){
-    //console.log('cslFetcher.fetchStyle');
-    if(zcreq.styleUrlObj.host == 'www.zotero.org'){
-        var filename = cslFetcher.cslDirName + '/' + zcreq.styleUrlObj.shortName + '.csl';
-        fs.readFile(filename, 'utf8', function(err, data){
-            if(err){
-                global.zcite.respondException(err, zcreq.response, 404);
-                return;
-            };
-            zcreq.cslXml = data;
-            callback(zcreq);
-        });
+cslFetcher.resolveZoteroShortName = function(zcreq, shortName){
+    //check if independent style from zotero repo
+    if((typeof this.cslShortNames[shortName] != 'undefined') && (this.cslShortNames[shortName] === true)){
+        var filename = this.cslDirPath + '/' + shortName + '.csl';
+        return filename;
     }
-    else{
-        var cslXml = '';
-        var fetchConn = http.createClient(80, urlObj.host);
-        var request = fetchConn.request('GET', urlObj.pathname,
-            {'host': urlObj.host});
-        request.on('response', function(response){
-            if(response.statusCode != 200){
-                throw {'message': 'Error fetching CSL'};
+    //check if dependent file from zotero repo
+    else if(typeof this.cslDependentShortNames[shortName] != 'undefined'){
+        //cached dependent style reference
+        if(typeof this.cslDependentShortNames[shortName] == "string"){
+            return this.resolveZoteroShortName(zcreq, this.cslDependentShortNames[shortName]);
+        }
+        //dependent style we haven't resolved before
+        else{
+            var filename = this.cslDirPath + '/dependent/' + shortName + '.csl';
+            
+        }
+    }
+};
+
+cslFetcher.fetchStyle = function(zcreq, callback){
+    try{
+        //console.log('cslFetcher.fetchStyle');
+        if(zcreq.styleUrlObj.host == 'www.zotero.org'){
+            //check if independent style from zotero repo
+            if((typeof this.cslShortNames[zcreq.styleUrlObj.shortName] != 'undefined') && (this.cslShortNames[zcreq.styleUrlObj.shortName] === true)){
+                var filename = cslFetcher.cslPath + '/' + zcreq.styleUrlObj.shortName + '.csl';
+                fs.readFile(filename, 'utf8', function(err, data){
+                    zcreq.cslXml = data;
+                    callback(err, zcreq);
+                });
             }
-            response.setEncoding('utf8');
-            response.on('data', function(chunk){
-                cslXml += chunk;
+            //check if dependent file from zotero repo
+            else if(typeof this.cslDependentShortNames[zcreq.styleUrlObj.shortName] != 'undefined'){
+                if(typeof this.cslShortNames[zcreq.styleUrlObj.shortName] == "string"){
+                    
+                }
+            }
+        }
+        else{
+            var cslXml = '';
+            var fetchConn = http.createClient(80, urlObj.host);
+            var request = fetchConn.request('GET', urlObj.pathname,
+                {'host': urlObj.host});
+            request.on('response', function(response){
+                if(response.statusCode != 200){
+                    throw {'message': 'Error fetching CSL'};
+                }
+                response.setEncoding('utf8');
+                response.on('data', function(chunk){
+                    cslXml += chunk;
+                });
+                response.on('end', function(){
+                    zcreq.cslXml = cslXml;
+                    callback(zcreq);
+                });
             });
-            response.on('end', function(){
-                zcreq.cslXml = cslXml;
-                callback(zcreq);
-            });
-        });
-        request.end();
+            request.end();
+        }
+    }
+    catch(err){
+        zcite.respondException(err, zcreq.response);
     }
 }
 
@@ -81,7 +133,24 @@ cslFetcher.loadStyle = function(style, callback, cargs){
     
 }
 
+cslFetcher.readDependent = function(xml){
+    var parser = this.parser;
+    var dStyle = parser.parseFromString(xml);
+    var linkEls = dStyle.getElementsByTagName("link");
+    for(var i = 0; i < linkEls.length; i++){
+        if(linkEls[i].getAttribute("rel") == "independent-parent"){
+            console.log("independent-parent found: " + linkEls[i].getAttribute("href"));
+            return linkEls[i].getAttribute("href");
+        }
+    }
+    return false;
+}
 
-exports.cslFetcher = cslFetcher;
+if (typeof module !== 'undefined' && "exports" in module) {
+    exports.cslFetcher = cslFetcher;
+}
+
+console.log("init cslFetcher");
+cslFetcher.init();
 
 
